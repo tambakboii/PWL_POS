532ea854bd12c845669bc9483bf6817791cc3943
athiyan aqil muhammad <110665066+tambakboii@users.noreply.github.com>
2024-03-13 19:33:33 +0700

    praktikum 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,9 +21,12 @@ class UserController extends Controller
 
         //$user = UserModel::firstWhere('level_id',1);
 
-        $user = UserModel::findOr(20,['username','nama'],function(){
-            abort(404);
-        });
+        //RSM
+        // $user = UserModel::findOr(20,['username','nama'],function(){
+        //     abort(404);
+        // });
+
+        $user = UserModel::where('username','manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
